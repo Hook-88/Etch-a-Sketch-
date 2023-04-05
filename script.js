181bf77fe14599faf6970f574c18919100f5f3c7
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-05 16:27:09 -0500

    Add check for less tha 100

--- script.js
+++ script.js
@@ -4,26 +4,39 @@ const gridContainer = document.createElement('div');
 gridContainer.id = 'grid-container';
 BODY.appendChild(gridContainer);
 
-function appendFieldsToContainer(numOfFields) {
-  for (let i = 0; i < numOfFields; i++) {
-    const field = document.createElement('div');
-    field.className = 'grid-field';
-    
-    field.addEventListener('mouseenter', changeColorToGrey);
-    //field.addEventListener('mouseleave', changeColorToWhite);
-
-    gridContainer.appendChild(field);
+function appendFieldsToContainer(gridSize) {
+  if (gridSize > 100) {
+    return 'the max is 100';
+  } else {
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+      const field = document.createElement('div');
+      let fieldSize = 100 / gridSize;
+  
+      fieldSize = convertToStringPercentage(fieldSize);
+      
+      setFieldStyles(field, fieldSize);
+      
+      field.addEventListener('mouseenter', changeColorToGrey);
+      //field.addEventListener('mouseleave', changeColorToWhite);
+  
+      gridContainer.appendChild(field);
+  }
   }
 }
-
-appendFieldsToContainer((16*16));
-
 
 
 function changeColorToGrey(e) {
   e.target.classList.add("charcoal");
 }
 
-function changeColorToWhite(e) {
-  e.target.classList.remove("charcoal");
+function convertToStringPercentage(number) {
+  return number.toString() + "%";
 }
+
+function setFieldStyles(field, fieldSize) {
+  field.style.height = fieldSize;
+  field.style.width = fieldSize;
+}
+
+appendFieldsToContainer(100);
+
