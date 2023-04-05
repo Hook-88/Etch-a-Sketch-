72ced3e5f54f3818b5a0a3dfcd7f9e56582f06d3
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-05 15:35:53 -0500

    Add 16x16 grid

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const BODY = document.querySelector('body');
 const gridContainer = document.createElement('div');
+
 gridContainer.id = 'grid-container';
+BODY.appendChild(gridContainer);
+
+function appendFieldsToContainer(numOfFields) {
+  for (let i = 0; i < numOfFields; i++) {
+    const field = document.createElement('div');
+    field.className = 'grid-field'
+    gridContainer.appendChild(field);
+  }
+}
+
+appendFieldsToContainer((16*16));
+
 
