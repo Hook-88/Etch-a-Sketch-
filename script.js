a0bc7671fbb34537ae125e27679c8e1aa5f25465
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-05 15:50:21 -0500

    Add pencil effect

--- script.js
+++ script.js
@@ -7,7 +7,11 @@ BODY.appendChild(gridContainer);
 function appendFieldsToContainer(numOfFields) {
   for (let i = 0; i < numOfFields; i++) {
     const field = document.createElement('div');
-    field.className = 'grid-field'
+    field.className = 'grid-field';
+    
+    field.addEventListener('mouseenter', changeColorToGrey);
+    //field.addEventListener('mouseleave', changeColorToWhite);
+
     gridContainer.appendChild(field);
   }
 }
@@ -15,3 +19,11 @@ function appendFieldsToContainer(numOfFields) {
 appendFieldsToContainer((16*16));
 
 
+
+function changeColorToGrey(e) {
+  e.target.classList.add("charcoal");
+}
+
+function changeColorToWhite(e) {
+  e.target.classList.remove("charcoal");
+}
